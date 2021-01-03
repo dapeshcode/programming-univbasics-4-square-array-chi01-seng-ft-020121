b5e9c2841b5c4690a0e3b984685445e7d75ec400
Alexandra Peccia <ampeccia@gmail.com>
2021-01-03 21:36:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@
 def square_array(array)
   count = 0
   new_nums = []
-  while array.length < count do
+  while count < array.length do
     x = array[count]
     new_nums.push(x**2)
     count += 1
@@ -10,5 +10,3 @@ def square_array(array)
   p new_nums 
 end
 
-test = [1, 2, 3]
-square_array(test)
