837bf591b1ace7d30ddf25b91338fe06e1d3493f
Alexandra Peccia <ampeccia@gmail.com>
2021-01-03 21:29:29 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   new_nums = []
   count = 0
   while array.length < count do
-    new_nums.push(array[count]**2)
+    new_nums.push(array[count] * array[count])
   end 
   
 end
